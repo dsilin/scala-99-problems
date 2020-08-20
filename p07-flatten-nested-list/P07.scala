d7dd4a92eb1ce53b3ede177311ef81a46edbd964
Dmitry Silin <dsilin11@gmail.com>
2020-08-20 23:39:51 +0300

    forgot to add description for task 7

--- p07-flatten-nested-list/P07.scala
+++ p07-flatten-nested-list/P07.scala
@@ -1,3 +1,8 @@
+// P07 (**) Flatten a nested list structure.
+//     Example:
+//     scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+//     res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+
 import scala.language.implicitConversions
 
 object P07 {
