6c1c1e3bfcf09f47d7bb623f8bbb13676d346e39
Dmitry Silin <dsilin11@gmail.com>
2020-08-23 18:32:41 +0300

    forgot description for problem 14

--- p14-duplicate-elements-of-list/P14.scala
+++ p14-duplicate-elements-of-list/P14.scala
@@ -1,3 +1,8 @@
+// P14 (*) Duplicate the elements of a list.
+//     Example:
+//     scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+//     res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+
 object P14 {
   def duplicate[A](list: List[A]) = list flatMap (el => List(el, el))
 
